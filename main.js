cc2e96131b38d940ab484075ee7b7d5424840963
tung.pham@ <tung.pham@pacom-solution.com>
2022-10-05 22:07:21 +0700

    add new vue

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data () {
+        return  {
+            teamName: "test duy tùng",
+            firstName: 'Tùng',
+            lastName: ' Phạm',
+            isHello: false
+        }
+    }
+});
+
+app.mount("#contact")
